2e4f7ade9ac52b7de613e52a06611c486db2dfe8
Callum Smith <callum@smither.org>
2022-03-10 23:37:43 +0000

    Try fix navbar

--- SecureRestrictions.php
+++ SecureRestrictions.php
@@ -53,7 +53,8 @@ function get_allowed_categories($strict = true)
 
 function limit_frontend_categories_to_allowed($query)
 {
-    if (!is_admin() && !current_user_can("administrator") && $query->is_main_query()) {
+    print_r($query);
+    if (!is_admin() && !current_user_can("administrator")) {
         // Not a query for an admin page.
         // It's the main query for a front end page of your site.
         $allowed_categories = get_allowed_categories();
