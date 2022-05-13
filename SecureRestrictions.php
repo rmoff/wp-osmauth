07d07f765d53a6c77b45a2a9dd9d3938afc247e0
Callum Smith <callum@smither.org>
2022-05-13 10:42:07 +0100

    Add debug for main loop filtering

--- SecureRestrictions.php
+++ SecureRestrictions.php
@@ -160,3 +160,15 @@ function wpse_77390_enable_media_categories()
     register_taxonomy_for_object_type('category', 'attachment');
     register_taxonomy_for_object_type('post_tag', 'attachment');
 }
+
+add_filter( 'the_content', 'filter_the_content_in_the_main_loop', 1 );
+function filter_the_content_in_the_main_loop( $content ) {
+ 
+    // Check if we're inside the main loop in a single Post.
+    if ( is_singular() && in_the_loop() && is_main_query() ) {
+        print_r($content);
+        return $content . esc_html__( 'I’m filtering the content inside the main loop', 'wporg');
+    }
+ 
+    return $content;
+}
