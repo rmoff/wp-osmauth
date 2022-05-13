c631d68b15799d8dde39cd1c974b7d5eb6a9feae
Callum Smith <callum@smither.org>
2022-05-13 11:37:10 +0100

    remove redundent admin bypass

--- SecureRestrictions.php
+++ SecureRestrictions.php
@@ -164,9 +164,6 @@ function wpse_77390_enable_media_categories()
 add_filter( 'the_content', 'filter_the_content_in_the_main_loop', 1 );
 function filter_the_content_in_the_main_loop( $content ) {
     // Check if we're inside the main loop in a single Post.
-    // if (is_admin() || current_user_can("administrator")) {
-    //     return $content;
-    // }s
     if ( is_singular() && in_the_loop() && is_main_query() ) {
         $allowed_categories = get_allowed_categories();
         $matches=[];
